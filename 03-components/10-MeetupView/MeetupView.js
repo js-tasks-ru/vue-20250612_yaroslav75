e57ec09b0af748a9d2e543f42b78f0495d09581a
javascriptru <67475495+jsru-1@users.noreply.github.com>
2025-07-01 19:51:10 +0300

    Added teacher solution for task: MeetupView

---
+++ 03-components/10-MeetupView/MeetupView.js
@@ -0,0 +1,48 @@
+import { defineComponent } from 'vue'
+import { UiAlert, UiContainer } from '@shgk/vue-course-ui'
+import MeetupAgenda from './MeetupAgenda.js'
+import MeetupDescription from './MeetupDescription.js'
+import MeetupCover from './MeetupCover.js'
+import MeetupInfo from './MeetupInfo.js'
+import './MeetupView.css'
+
+export default defineComponent({
+  name: 'MeetupView',
+
+  components: {
+    MeetupAgenda,
+    MeetupDescription,
+    MeetupCover,
+    MeetupInfo,
+    UiAlert,
+    UiContainer,
+  },
+
+  props: {
+    meetup: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  template: `
+    <div>
+      <MeetupCover :title="meetup.title" :image="meetup.image" />
+      <UiContainer>
+        <div class="meetup">
+          <div class="meetup__content">
+            <h2>Описание</h2>
+            <MeetupDescription :description="meetup.description" />
+            <h2>Программа</h2>
+            <MeetupAgenda v-if="meetup.agenda.length" :agenda="meetup.agenda" />
+            <UiAlert v-else>Программа пока пуста...</UiAlert>
+          </div>
+          <div class="meetup__aside">
+            <MeetupInfo :organizer="meetup.organizer" :place="meetup.place" :date="meetup.date" />
+            <div class="meetup__aside-buttons"></div>
+          </div>
+        </div>
+      </UiContainer>
+    </div>
+  `,
+})
