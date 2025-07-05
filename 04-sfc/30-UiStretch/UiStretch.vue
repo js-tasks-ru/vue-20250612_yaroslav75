314f6c67834fb50adecaa9bee41960c7913d448e
javascriptru <67475495+jsru-1@users.noreply.github.com>
2025-07-05 22:12:13 +0300

    Added teacher solution for task: UiStretch

---
+++ 04-sfc/30-UiStretch/UiStretch.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+
+<template>
+  <div class="stretch-container">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.stretch-container > * {
+  width: 100%;
+  height: 100%;
+}
+
+.stretch-container > :slotted(img),
+.stretch-container > :slotted(video),
+.stretch-container > :slotted(picture) {
+  object-fit: scale-down;
+}
+</style>
